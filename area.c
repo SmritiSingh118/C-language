58ef61bb0abcb3edf367a15b438f5cfcf7cf21f6
SmritiSingh118 <thesmritisingh11@gmail.com>
2026-01-16 22:46:16 +0530

    Implement triangle area calculation in area.c

---
+++ area.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    int h,b,area;
+    printf("input height and base of triangle:");
+    scanf("%d%d",&h,&b);
+    area = 0.5*h*b;
+    printf("area of triangle is %d",area);
+    
+}
