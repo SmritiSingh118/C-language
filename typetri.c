e5378b3fa23485de170c90aeed13661de99f2b26
SmritiSingh118 <thesmritisingh11@gmail.com>
2026-01-21 23:02:17 +0530

    Implement triangle type classification
    Add triangle type determination based on side lengths

---
+++ typetri.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+  float s1,s2,s3;
+  printf(" enter s1 , s2 and s3");
+  scanf("%f%f%f",&s1,&s2,&s3);
+  if(s1==s2 && s1==3)
+{
+printf("equilateral triangle");
+}
+  else if(s1==s2 || s1==s3 || s2==s3)
+{
+printf("isosceles triangle");
+}
+else
+{
+printf("scalene triangle");
+}
+}
