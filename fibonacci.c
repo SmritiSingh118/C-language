b572ec9b20c44fed3899e5b171ac4d2facc38f32
Smriti Singh <thesmritisingh11@gmail.com>
2026-02-22 11:53:56 +0530

    Add Fibonacci sequence generator in fibonacci.c
    Implement Fibonacci sequence calculation and output.

---
+++ fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{int n,a,b,c=0,i;
+printf("enter the number");
+scanf("%d",&n);
+if(n==1||n==0){
+    printf("%d",n);
+}
+else{
+    a=0;
+    b=1;
+    printf("01");
+    for(i=3;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+        printf("%d",c);
+    }
+}
+return 0;
+
+}
