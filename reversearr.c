6881f49af94097c4230d1dc2624b6d39b5b78647
Smriti Singh <thesmritisingh11@gmail.com>
2026-03-02 23:20:41 +0530

    Update reversearr.c

--- reversearr.c
+++ reversearr.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int reverse( int a[],int n){
     int i,t;
-    for (i=0;i<n/2;i++){
+    for (i=0;i<n/2;i++)
+    {
         t=a[i];
         a[i]=a[n-1-i];
         a[n-1-i]=t;
