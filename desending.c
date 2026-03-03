e369b6f399a3ec896d0494d71cfab5c6c79f4e06
Smriti Singh <thesmritisingh11@gmail.com>
2026-03-03 16:01:00 +0530

    Create desending.c

---
+++ desending.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int desending(int a[],int n){
+    int i,j,t;
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if (a[i]<a[j]){
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;}
+        }
+    }printf("sorted array:");
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }printf("\n");
+    return 0;
+}
+int main() 
+{int i, n;
+printf("Enter the number of elements: ");
+scanf("%d", &n);
+int a[n]; 
+for (i = 0; i < n; i++)
+{printf("Enter element %d: ", i + 1);
+scanf("%d", &a[i]);}
+printf("Original array: ");
+for (i = 0; i < n; i++)
+{printf("%d ", a[i]);}
+printf("\n");
+desending(a, n);
+return 0;
+}
