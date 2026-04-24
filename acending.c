7333c4137c4622310bc09950f418ef1f8e747480
Smriti Singh <thesmritisingh11@gmail.com>
2026-04-24 23:32:29 +0530

    Update acending.c

--- acending.c
+++ acending.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-int acending(int a[],int n){
-    int i,j,t;
+ #include <stdio.h>
+ int acending(int a[],int n){
+     int i,j,t;
     for(i=0;i<n;i++){
         for(j=i+1;j<n;j++){
             if (a[i]>a[j]){
