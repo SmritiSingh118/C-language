e1d1aa369bba8bb536e4af20154cf3bcb75726cf
Smriti Singh <thesmritisingh11@gmail.com>
2026-05-15 22:36:14 +0530

    Add grade calculation program in grade.c

---
+++ grade.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int marks;
+    printf("enter marks");
+    scanf("%d",&marks);
+    if(marks>90)
+    {
+        printf("grade A");
+    }
+    else if(marks>50)
+    {
+        printf("grade B");
+    }
+    else if(marks>30)
+    {
+        printf("grade C");
+    }
+    else
+    {
+        printf("fail");
+    }
+    
+    return 0;
+}
