6ed4c2e31ba6f16547a5457d3435a609a77b81ad
Smriti Singh <thesmritisingh11@gmail.com>
2026-05-15 22:35:39 +0530

    Add profit_loss.c to calculate profit or loss

---
+++ profit_loss.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int cp,sp;
+    printf("enter cp and sp");
+    scanf("%d%d",&cp,&sp);
+    if(sp>cp)
+    {
+        printf("profit");
+    }
+    else if(cp>sp)
+    {
+        printf("loss");
+    }
+    return 0;
+}
