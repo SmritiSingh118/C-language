dcb155302ae140dcacf97ed64c57968e4e3687cb
Smriti Singh <thesmritisingh11@gmail.com>
2026-05-15 22:37:18 +0530

    Add program to check if a number is three digits

---
+++ 3digit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a number");
+    scanf("%d",&a);
+    if(a>=100&&a<=999)
+    {
+        printf("three digit number");
+    }
+    else
+    {
+        printf("not a three digit number");
+    }
+    return 0;
+}
